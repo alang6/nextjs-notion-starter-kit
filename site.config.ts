772e79cf8ff26353c0f841e4fa2dfad3b08942d0
alang6 <alang6@users.noreply.github.com>
2023-04-22 16:05:06 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '7db85f3f3fcb418f9876e3641bb0fd49',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Next.js Notion Starter Kit学习',
+  domain: '0086.eu.org',
+  author: '3399',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
